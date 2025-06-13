00e9588c5179d3bf8abac9356ec4017b7ed8684e
Zeel-AU-FEST <zeel3165.patel@gmail.com>
2025-06-13 15:04:14 +0530

    updated winning condition for lottery game

--- LotteryGame/src/App.jsx
+++ LotteryGame/src/App.jsx
@@ -2,8 +2,12 @@ import './App.css'
 import Lottery_Revised from "./Components/Lottery_Revised.jsx"
 import TicketNumber from './Components/TicketNumber.jsx'
 import Ticket from './Components/Ticket.jsx';
+import {Sum} from "./Components/helper.js"
 function App() {
-  
-  return <Lottery_Revised n={3} winningsum={15}></Lottery_Revised>
+  let winningcondition=(ticket)=>
+  { 
+    return ticket[0]==0;
+  }
+  return <Lottery_Revised n={3} winningcondition={winningcondition}></Lottery_Revised>
 }
 export default App
--- LotteryGame/src/Components/Lottery_Revised.jsx
+++ LotteryGame/src/Components/Lottery_Revised.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { generateTicket, Sum } from "./helper.js";
 import Ticket from "./Ticket.jsx";
 import "./lottery_Revised.css"
-export default function Lottery_Revised({n=3,winningsum=15})
+export default function Lottery_Revised({n=3,winningcondition})
 {
     let [ticket, Setticket] = useState(generateTicket(n));
-    let iswinning=Sum(ticket);
+    let iswinning=winningcondition(ticket)
     function ticket_generator()
     {
         Setticket(generateTicket(n));
@@ -13,9 +13,9 @@ export default function Lottery_Revised({n=3,winningsum=15})
     return<>
              <h1>Lottery Game</h1>
               <Ticket ticket={ticket}/>
-              {
-                iswinning==winningsum?<p>Congratulations You won the Lottery</p>:null
-              }
+              
+               { iswinning==true? <p>Congratulations You won the Lottery</p>:null}
+              
              <button onClick={ticket_generator}>Buy Ticket</button>
     </>
 }
